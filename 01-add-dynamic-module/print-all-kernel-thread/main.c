e18e6b313fd2ad281d919da1e10d282462fb1e16
ashenye <luren145@gmail.com>
2023-10-23 23:24:59 +0800

    lab1-1

---
+++ 01-add-dynamic-module/print-all-kernel-thread/main.c
@@ -0,0 +1,27 @@
+#include <linux/init.h>
+#include <linux/init_task.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+#include <linux/sched/task.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("ashenye");
+MODULE_DESCRIPTION("print all kernel threads info");
+MODULE_VERSION("0.1");
+
+static int __init print_info_init(void) {
+  printk(KERN_ALERT "Hello: Hello, World!\n");
+  struct task_struct *p = &init_task;
+  for_each_process(p) if (p->mm == NULL) {
+    printk(KERN_INFO "print-kernel-threads-info: "
+                     "name: %s, pid: %d, state: %ld, prio: %d, parent: %d",
+           p->comm, p->pid, p->stats, p->prio, p->parent->pid);
+  }
+  return 0;
+}
+
+static void __exit print_info_exit(void) {}
+
+module_init(print_info_init);
+module_exit(print_info_exit);
